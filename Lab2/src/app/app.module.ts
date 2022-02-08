3bef01221038c21bb1bc85513623abda98c7fe6c
Brett Whittington <brett.whittington@cgi.com>
2022-02-08 15:35:51 -0500

    revert

--- Lab2/src/app/app.module.ts
+++ Lab2/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HelloWorldComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
